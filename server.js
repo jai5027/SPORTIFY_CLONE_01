ce822181ba76a5d84325bcf57ec9fba6273a3072
Jai Sharma <jaisharma27868@gmail.com>
2026-03-06 22:11:43 +0530

    update code

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import app from './src/app.js'
 import dotenv from 'dotenv'
 dotenv.config()
+import connectDB from './src/db/db.js'
 
+connectDB()
 app.listen(3000, () => {
     console.log(3000);
 })
--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
 import express from 'express'
+import authRouter from './router/auth.router.js'
+import musicRouter from './router/music.router.js'
+import cookieParser from 'cookie-parser'
 
 const app = express()
+app.use(express.json())
+app.use(cookieParser())
+
+app.use('/api/auth', authRouter)
+app.use('/api/music', musicRouter)
 
 export default app
